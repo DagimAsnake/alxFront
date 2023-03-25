6dea46187e8ad39c8098646477d43b0d5a1cf023
Dagim Asnake <dagimasnake1@gmail.com>
2023-03-25 00:10:46 -1200

    add useEffect

--- src/Components/Tables/TableOne.js
+++ src/Components/Tables/TableOne.js
@@ -1,40 +1,56 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const TableOne = () => {
+
+  useEffect(() => {
+    const datafetch = async () => {
+      const response = await fetch("http://localhost:8080/", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    };
+    datafetch();
+  }, []);
+
     return(
+
         <div>
-          <h1>Table one</h1>
-            <table class="table table-hover">
-  <thead className="table-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>fjklsa</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        </div>
+        <h1>Table one</h1>
+          <table class="table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">first</th>
+              <th scope="col">second</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry the Bird</td>
+              <td>fjklsa</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+          </table>
+      </div>
+      
     )
 }
 
